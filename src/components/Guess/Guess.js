1d762d1ac3696cc4e025a24e80e7a32fd0885cd4
Jeff Bridgforth <jeff.bridgforth@gmail.com>
2024-07-25 13:28:51 -0400

    Exercise 4 complete

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
 
-function Guess({value}) {
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : 'cell';
+  return <span className={className}>{letter}</span>;
+}
+
+function Guess({ value, answer }) {
+
+  const correctAnswer = checkGuess(value, answer);
+
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key="{num}" className="cell">
-          { value ? value[num] : undefined }
-        </span>
+        <Cell
+          key={num}
+          letter={correctAnswer ? correctAnswer[num].letter : undefined}
+          status={correctAnswer ? correctAnswer[num].status : undefined}
+        />
       ))}
     </p>
   )
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -5,13 +5,13 @@ import { range } from '../../utils';
 
 import Guess from '../Guess';
 
-function GuessResults({guesses}) {
+function GuessResults({guesses, answer}) {
   return (
     <>
       <div className="guess-results">
 
         {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
-            <Guess key="{num}" value={guesses[num]} />
+          <Guess key={num} value={guesses[num]} answer={answer} />
         ))}
 
       </div>
